c11c12af994f97a21a52a6da85a4a37420ee80f2
jeredam <usr3593@salesianos-lacuesta.net>
2023-05-29 13:19:55 +0200

    Feat(mob): Se añadieron las clases hostilemobs y pacificmobs con sus respectivos metodos

---
+++ com/mojang/mob/pacificmob/PacificMob.java
@@ -0,0 +1,18 @@
+package com.mojang.mob.pacificmob;
+
+import com.mojang.interfaces.Damage;
+import com.mojang.mob.Mob;
+
+public abstract class PacificMob extends Mob implements Damage{
+
+    public PacificMob(int mobHealth) {
+        super(mobHealth);
+    }
+    
+    @Override
+    public void getAttack(int attack){
+        int currentHealth = this.mobHealth - attack;
+        System.out.println("Salud restante: " + currentHealth);
+    }
+
+}
