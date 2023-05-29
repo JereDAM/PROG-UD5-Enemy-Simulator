bbb783b71ab309a56c16843b475516641b5eb378
jeredam <usr3593@salesianos-lacuesta.net>
2023-05-29 13:38:32 +0200

    Feat(Sheep): Se añadio oveja como mob pacifico.

---
+++ com/mojang/mob/pacificmob/Sheep.java
@@ -0,0 +1,16 @@
+package com.mojang.mob.pacificmob;
+
+public class Sheep extends PacificMob{
+
+    public Sheep(int mobHealth) {
+        super(mobHealth = 5);
+    }
+
+    @Override
+    public void move() {
+        System.out.println("AYUDAAAAAA *SOLLOZA* LLAMEN A LA POLICIAAA ME ESTÁN MATANDOOOOO *LLORA*");
+    }
+
+    
+    
+}
